94565287c428c014ab1a4d2598208d4e008dce30
Insomnia <jamie.mar.jones14@gmail.com>
2020-10-08 09:51:17 -0400

    filled out the server a bit

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
 const express = require("express");
 const mysql = require("mysql");
-const exhb = require("express-handlebars");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
